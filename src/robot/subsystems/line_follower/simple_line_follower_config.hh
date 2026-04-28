f03a101acc72f5c0637534ee9c3e36b99b8980e7
Chair <A01708525@tec.mx>
2026-04-28 05:17:00 -0600

    Good runs at speed 350 with SLF

--- src/robot/subsystems/line_follower/simple_line_follower_config.hh
+++ src/robot/subsystems/line_follower/simple_line_follower_config.hh
@@ -4,11 +4,11 @@
 
 namespace pll::config {
 
-constexpr float    SLF_KP                  = 0.37f;
-constexpr float    SLF_KD                  = 1.955f;
-constexpr int16_t  SLF_BASE_SPEED          = 240;
+constexpr float    SLF_KP                  = 0.40f;
+constexpr float    SLF_KD                  = 2.7f;
+constexpr int16_t  SLF_BASE_SPEED          = 350;
 constexpr int16_t  SLF_MOTOR_MAX_CMD       = 400;
-constexpr uint16_t SLF_CALIBRATION_SAMPLES = 100;
+constexpr uint16_t SLF_CALIBRATION_SAMPLES = 250;
 constexpr int16_t  SLF_CALIBRATION_SPEED   = 60;
 constexpr bool     SLF_FOLLOW_WHITE        = true;
 
